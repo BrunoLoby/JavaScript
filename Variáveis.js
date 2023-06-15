bf12a538a0a2dce254d461c0641a40c255f4301d
Bruno <bruno.lobianco19@gmail.com>
2023-06-15 13:07:41 -0300

    first commit

---
+++ Variáveis.js
@@ -0,0 +1,33 @@
+//Variáveis
+
+let idade = 26; //Criando primeira variável
+
+console.log(idade)
+
+//tipos de variáveis
+
+let numero = 30     //number
+idade = 30          //number
+let altura = 1.85   //number
+let nome = "Bruno"  //string (texto)
+let estudando = true //booleano (true or false)
+
+
+console.log(numero, typeof numero)
+console.log(nome, typeof nome)
+console.log(altura, typeof altura)
+console.log(estudando, typeof estudando)
+
+let outra;
+
+console.log(outra)
+
+let nula = null;
+
+console.log(nula)
+
+var variável =30; //não utilize o var
+
+const constnte = 100
+
+
