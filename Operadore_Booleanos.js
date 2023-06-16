3a316437a257e9456a20f8a89aa3bc32e27b746a
Bruno <bruno.lobianco19@gmail.com>
2023-06-16 15:24:01 -0300

    atualizações

---
+++ Operadore_Booleanos.js
@@ -0,0 +1,25 @@
+//> Operadores Booleanos
+
+const numero =10;
+
+console.log(numero==10)
+console.log(numero >20)
+
+console.log(numero==10)
+console.log(10== '10')
+console.log(10=== '10')
+
+
+console.clear()
+
+console.log(10!= 10)
+console.log(10 !='10')
+console.log(10!=='10')
+
+let idade = 36
+let tenhoCnh = true
+
+const possoDirigir =idade >=18 && tenhCnh=== true
+
+console.log('Posso dirigir', PossoDirirgir)
+
