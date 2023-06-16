4d9ec966114ccab66bfc7f29ca1da3684424e2d7
Bruno <bruno.lobianco19@gmail.com>
2023-06-15 22:29:15 -0300

    Atualizações

--- Operadores_Aritméticos.js
+++ Operadores_Aritméticos.js
@@ -4,5 +4,26 @@ let numero2 = 2;
 
 const soma = numero1 + numero2
 
-console.log ('soma =', soma)
-console.log ('soma=', numero1-numero2)
+console.log('soma =', soma)
+console.log('subtração=', numero1 - numero2)
+console.log('Multiplicação=', numero1 * numero2)
+console.log('Divisão=', numero1 / numero2)
+console.log('Exponenciação=', numero1 ** numero2)
+console.log('Resto da Divisão=', numero1 % numero2)
+
+
+console.log(3 + 7 * 3)
+console.log((10 + 7 + 8) / 3)
+
+console.log(Math.PI)
+
+let horaDeAcrodar = 7
+if (horaDeAcrodar > 7) {
+    console.log("Perdeu a Hora")
+
+} else {
+    console.log("Acordou a Tempo")
+
+}
+
+
