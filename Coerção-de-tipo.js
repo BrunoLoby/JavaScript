66c2b1be1042cc27f1e605306f29c44168e044e5
Bruno <bruno.lobianco19@gmail.com>
2023-06-19 19:27:45 -0300

    Update

---
+++ Coerção-de-tipo.js
@@ -0,0 +1,29 @@
+
+
+const numero=10;
+
+console.log(numero,typeof numero)
+
+const numeroEmformatodeString = String(numero)
+console.log(numeroEmformatodeString, typeof numeroEmformatodeString)
+
+console.log(Number('1231234'))
+console,log(parseFloat('23224.234'))
+console,log(parseInt('23224.234'))
+console,log(Boolean(0))
+
+console.log("10"+1)
+console.log('10'-1)
+console.log(10-"ssss") //retorna NaN = Not a number
+
+
+let n = 1+"1"
+n+n-1;
+
+console.log(2+3+4+"5");
+console.log("5"+2+3+4);
+console.log("10"-"4"-"3"-2+"5")
+
+
+
+
