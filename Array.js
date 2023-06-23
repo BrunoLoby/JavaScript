c93b9dcdfa393484ded9b93219fec5c5f819d177
Bruno <bruno.lobianco19@gmail.com>
2023-06-22 22:02:20 -0300

    Update

---
+++ Array.js
@@ -0,0 +1,13 @@
+// Como criar um array?
+
+let arr = ['Bruno',18,1.78,true]
+
+console.log(arr)
+
+// Como acessar os elementos do array
+
+console.log('Primeiro Elemento',arr[0])
+console.log('Segundo Elemento',arr[1])
+console.log('Terceiro Elemento',arr[2])
+console.log('Quarto Elemento',arr[3])
+
